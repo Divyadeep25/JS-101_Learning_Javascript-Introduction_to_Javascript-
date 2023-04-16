48fdbd38dd12b3e7fc9726112f587c24696c8fdd
Divyadeep25 <ydeep641@gmail.com>
2023-04-16 13:28:45 +0530

    change name

---
+++ problem1.js
@@ -0,0 +1,6 @@
+let a ="Masai School";
+
+let b="A Transformation in Education";
+
+console.log(a);
+console.log(b);
---
+++ problem2.js
@@ -0,0 +1,8 @@
+let name;
+name="Divyadeep";
+
+console.log(name);
+name1="Bandu";
+console.log(name1);
+name3= "Waghmare";
+console.log(name3);
